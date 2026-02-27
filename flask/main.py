4b2f6d82a22995519afee6ab86b2b1e185466bd2
erlyg <erlyginegor@gmail.com>
2026-02-27 18:46:44 +0300

    доработаная 1 задача

--- flask/main.py
+++ flask/main.py
@@ -3,15 +3,11 @@ from flask import Flask, url_for, render_template, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-@app.route('/d')
-def main():
-    return "sajsdj"
-
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/index/<name>')
+def index(name='ыввавв'):
     print('dsf')
-    return render_template('index.html', title='Заготовка')
+    return render_template('index.html', title=name)
 
 if __name__ == '__main__':
     app.run(port=8081, host='127.0.0.1')
