2d70890b27a1e0f06e6dc8408431e103913e044f
erlyg <erlyginegor@gmail.com>
2026-02-27 19:54:20 +0300

    Первое ДЗ

--- flask/main.py
+++ flask/main.py
@@ -3,11 +3,18 @@ from flask import Flask, url_for, render_template, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+
 @app.route('/')
 @app.route('/index/<name>')
-def index(name='ыввавв'):
-    print('dsf')
+def index(name='title'):
     return render_template('index.html', title=name)
+
+
+@app.route("/distribution")
+def distribution():
+    user_list = ['Ваня', 'Петя', 'Саша', 'Кирилл']
+    return render_template('index2.html', user_list=user_list)
+
 
 if __name__ == '__main__':
     app.run(port=8081, host='127.0.0.1')
