cd50995ec7aba542ef6fca8fa139656ae56ab1f6
erlyg <erlyginegor@gmail.com>
2026-03-22 11:58:17 +0300

    запрос 3

--- flask/request_3.py
+++ flask/request_3.py
@@ -1,15 +1,13 @@
-"""
 from flask import Flask
 from data import db_session
 from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-#db = "mars_explorer.sqlite"
+# db = "mars_explorer.sqlite"
 db = input().split('.')
 db_session.global_init("db/" + db[0] + ".db")
 db_sess = db_session.create_session()
 for user in db_sess.query(User).all():
     if user.age < 18:
         print(user)
-"""
